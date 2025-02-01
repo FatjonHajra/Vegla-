f87e3cb710bce6ed8e5dfaa2b957fa38dfe0da06
FatjonHajra <fationhajra@icloud.com>
2025-02-01 18:08:22 +0100

    Update detyra1.cpp

--- detyra1.cpp
+++ detyra1.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-
     char rez;
     int totaliPikeve = 0;
 
     for (int i = 0; i < 5; i++)
     {
-        cout << "Shenoni rezultatin "<< 1+i<<": F, B, H: ";
-        cin >> rez;
-
-        switch (rez)
+        bool inputValide = false;
+        while (!inputValide) 
         {
-        case 'H':
-            totaliPikeve += 0;
-            break;
-        case 'F':
-            totaliPikeve += 3;
-            break;
-        case 'B':
-            totaliPikeve += 1;
-            break;
-        default:
-            cout << "Shkronje jo valide" << endl;
-            i--;
+            cout << "Shenoni rezultatin per ndeshjen " << i + 1 << " (F, B, H): ";
+            cin >> rez;
+            rez = toupper(rez);
+
+            switch (rez)
+            {
+            case 'H':
+                totaliPikeve += 0;
+                inputValide = true;
+                break;
+            case 'F':
+                totaliPikeve += 3;
+                inputValide = true;
+                break;
+            case 'B':
+                totaliPikeve += 1;
+                inputValide = true;
+                break;
+            default:
+                cout << "Gabim! Ju lutem vendosni F, B ose H.\n";
+            }
         }
     }
 
+    cout << "\n-------------------------\n";
     cout << "Totali i pikeve: " << totaliPikeve << endl;
-    cout << "Piket per ndeshje: " << (float)totaliPikeve / 5 << endl;
+    cout << "Piket mesatare per ndeshje: " << (float)totaliPikeve / 5 << endl;
+    cout << "-------------------------\n";
 
     return 0;
 }
+
